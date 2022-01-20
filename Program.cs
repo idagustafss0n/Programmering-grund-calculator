d7526d5231d2dfbb0b109daa888cf3e662a8f2a3
Ida Gustafsson <93993079+idagustafss0n@users.noreply.github.com>
2022-01-20 11:31:52 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,29 +17,8 @@ namespace Calculator
 
                 double tal1 = double.Parse(Console.ReadLine());
 
-                //string inmatat1 = Console.ReadLine();
-                //double tal1 = double.Parse(inmatat1);
-
-
                 Console.WriteLine("Välj räknesätt: + | - | * | / ");
                 Console.WriteLine("A.  Avsluta");
-
-
-                /* Frågan om tal2 är inflyttat i varje case. Detta för att kunna få ett snabbare svar om användaren väljer Marcus eller avsluta. Detta medför dock att 
-                   koden blir längre och många rader som ser likadana ut. Det hade gått att kontrollera om användaren skrev in Marcus med en if-sats innan switch och om så är 
-                   fallet avsluta koden innan switchen.*/
-
-                /* Det går även att göra miniräknaren med flera if-satser. Men jag läste att om det är många if-satser efter varandra som liknar varandra så 
-                   rekomenderas en annan metod. */
-
-                /* Då miniräknaren inte har ett visst antal uträkningar innan den tar stopp använder jag lista istället för array  */
-
-                /* Tryparse eller try catch borde ligga med där användaren fyller i information. För att undvika att programmet kraschar*/
-
-                /* De tre case med + - * ser likadana ut i grunden men det skiljer i uträkning och hur den lägger in i listan. Det borde gå 
-                   att göra flera case men med samma action. Som med "Marcus" och "MARCUS" där koden gör samma sak oavsett vilken inmatning. 
-                   Däremot så hade det krävts if-satser för att sära på räknesätten. Så mitt beslut blev att ha flera olika case även fast mycket 
-                   av koden är densamma. Jag tycker att koden blir lättare att läsa och få en överblick på, även fast den är lite längre. */
 
                 menu = Console.ReadLine();
 
@@ -51,7 +30,6 @@ namespace Calculator
 
                         double summan = tal1 + tal_addition;
 
-                        /*Skickar in hela uträkningen i listan. Som en string för att få med hela uträkningen. */
                         lista.Add(tal1+"+"+ tal_addition + "="+summan);
 
                         Console.WriteLine("Summan av tal " + tal1 + " + " + tal_addition + " = " + summan);
@@ -102,8 +80,7 @@ namespace Calculator
 
                         break;
 
-                        /* I Case med division ligger en if-sats som kontrollerar så inte täljare eller nämnare är 0
-                            Här hade det gått att lägga en if-sats i början av case för att direkt plocka om tal1 är 0. Om inte tal1 är 0 så gå vidare och be om tal2*/
+                        
                     case "/":
                         Console.WriteLine("Tal 2: ");
                         double tal_division = double.Parse(Console.ReadLine());
